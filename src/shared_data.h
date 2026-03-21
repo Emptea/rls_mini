932f60655fa01bd6b7a0a8fa6a9232255be8b68b
Emptea <aryas1725@gmail.com>
2026-03-21 10:45:29 +0000

    config changed: pps + tx visible on oscill

--- src/shared_data.h
+++ src/shared_data.h
@@ -22,18 +22,18 @@ public:
 	const u220_config_t u220_config = {
 		.rate       = 5e6,
 		.freq       = 3.6e9,
-		.tx_gain    = {0,    0   },
+		.tx_gain    = {70,   70  },
 		.rx_gain    = {0,    0   },
 		.tx_bw      = {56e6, 56e6},
 		.rx_bw      = {56e6, 56e6},
 		.ref        = "internal",
-		.cpu_format = "sc16",
+		.cpu_format = "fc32",
 		.otw_format = "sc16",
-		.pps        = "internal",
+		.pps        = "external",
 		.tx_spb     = 0,
 		.rx_spb     = 0
     };
-	const PIString u220_args = "recv_frame_size=4096,num_recv_frames=128,send_frame_size=8192,num_send_frames=256";
+	const PIString u220_args = "recv_frame_size=4104,num_recv_frames=128,send_frame_size=8192,num_send_frames=256";
 
 	PIVector<size_t> active_boards;
 	static GlobalData * instance();
