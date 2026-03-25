bdc6ca6c14b6e061f9072791663b49bd5c2c506b
Emptea <aryas1725@gmail.com>
2026-03-25 13:07:42 +0300

    changed reception start moment

--- src/shared_data.cpp
+++ src/shared_data.cpp
@@ -95,7 +95,7 @@ void GlobalData::start() {
 	startEth();
 	for (size_t i = 0; i < active_boards.size(); i++) {
 		// u220_ptrs[active_boards[i]]->start_reception(4.64+180*0.2e-6);
-		u220_ptrs[active_boards[i]]->start_reception(4.64 - 52 * 0.2e-6);
+		u220_ptrs[active_boards[i]]->start_reception(4.64 - 52 * 0.2e-6 - 46.4e-5);
 		u220_ptrs[active_boards[i]]->start_transmission(4.64);
 	}
 }
